4917143484edabb362f7bad22e1725385b511591
kgenezv <kgenezv@eafit.edu.co>
2025-09-20 19:59:50 -0500

    Update load.py

--- src/data/load.py
+++ src/data/load.py
@@ -56,7 +56,7 @@ def load_and_log():
                 torch.save((x, y), file)
 
         # ✍️ Save the artifact to W&B.
-        run.log_artifact(raw_data) #KEVIN
+        run.log_artifact(raw_data)
 
 # testing
 load_and_log()
